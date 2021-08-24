56bd989d70790d87188b8c01430fa94644ad4642
Mir Tanvir Ahmed <mrmirofficial@gmail.com>
2021-08-24 22:41:05 +0600

    only front camera

--- index2.js
+++ index2.js
@@ -14,29 +14,12 @@ let scene,
     loader,
     mesh;
 
-const GLTF_Loader = new GLTFLoader();
 const FBX_Loader = new FBXLoader();
 
-const defaultCamera = { x: 0, y: 10, z: 40 };
-const objDefaultPosition = { x: 0, y: -2, z: 0 };
-const objectDefaultScale = { x: 1, y: 1.2, z: 1 };
+const defaultCamera = { x: 0, y: 6, z: 25 };
 const animationTime = 1000;
 
 let children = {};
-
-const billboards = {
-    middle: {
-        path: './models/textures/billboard_middle.jpg',
-        position: { x: 0, y: 2.5, z: 8 },
-        name: "middle",
-        circle: {
-            normal: './models/gltf/circle_1.gltf',
-            onHover: './models/gltf/circle_2.gltf',
-            position: { x: 17, y: -3, z: -10 },
-        }
-    }
-};
-
 
 const myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
     keyboard: false,
@@ -66,15 +49,15 @@ const init = () => {
     let light = new THREE.AmbientLight(0x404040); // soft white light
     scene.add(light)
 
-
-    // 
-    makeBanner();
-    makeBillboards();
-
-
     setupFloor();
 
 
+    FBX_Loader.load("./models/fbx/full.fbx", fbx => {
+        console.log(fbx.children.filter(c => c.name == 'polySurface9'));
+        fbx.position.set(0, -3, 10);
+        fbx.scale.set(2, 2, 2);
+        scene.add(fbx);
+    }, undefined, err => console.log(err));
 
     window.addEventListener('resize', onWindowResize);
     window.addEventListener("hidden.bs.modal", handleModalClose);
@@ -97,10 +80,10 @@ const setupControls = () => {
     //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
 
     controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
-    controls.dampingFactor = 0.05;
-    controls.screenSpacePanning = false;
-    controls.maxDistance = defaultCamera.z;
-    controls.maxPolarAngle = Math.PI / 2;
+    // controls.dampingFactor = 0.05;
+    // controls.screenSpacePanning = false;
+    // controls.maxDistance = defaultCamera.z;
+    // controls.maxPolarAngle = Math.PI / 2;
 
 }
 
@@ -131,102 +114,21 @@ const setupCamera = () => {
 }
 
 const handleModalClose = () => {
-
-    myModal.hide();
-    $(document.body).removeClass("modal-open");
-    $(".modal-backdrop").remove();
-
-
     new TWEEN.Tween(camera.position)
         .to({
             x: defaultCamera.x,
             y: defaultCamera.y,
             z: defaultCamera.z,
         }, animationTime)
-        .onComplete(() => {
-            camera.lookAt(0, 0, 0);
-        })
         .start();
 
 
+    myModal.hide();
+    $(document.body).removeClass("modal-open");
+    $(".modal-backdrop").remove();
+
 }
 
-
-const makeBanner = () => {
-
-    loader.load(
-        './models/textures/banner.jpg',
-        texture => {
-            material = new THREE.MeshBasicMaterial({ map: texture });
-            geometry = new THREE.PlaneGeometry(55, 20);
-            mesh = new THREE.Mesh(geometry, material);
-            mesh.position.set(0, 14, -5);
-            scene.add(mesh)
-        },
-        undefined, (err) => console.error('An error happened.')
-    );
-}
-
-const makeBillboards = () => {
-
-    for (const a in billboards) {
-        let billboard = billboards[a];
-        loader.load(
-            billboard.path,
-            texture => {
-                material = new THREE.MeshBasicMaterial({ map: texture });
-                geometry = new THREE.PlaneGeometry(28, 12);
-                mesh = new THREE.Mesh(geometry, material);
-                mesh.name = billboard.name;
-                mesh.position.set(
-                    billboard.position.x,
-                    billboard.position.y,
-                    billboard.position.z
-                );
-                scene.add(mesh)
-            },
-            undefined, (err) => console.error('An error happened.')
-        );
-
-        GLTF_Loader
-            .load(
-                billboard.circle.normal,
-                (gltf) => {
-                    gltf.scene.position.set(
-                        billboard.circle.position.x,
-                        billboard.circle.position.y,
-                        billboard.circle.position.z
-                    );
-                    gltf.scene.scale.set(3, 2.5, 2.5);
-                    scene.add(gltf.scene);
-                    gltf.scene.children[0].name = `circle-${billboard.name}-normal`;
-                },
-                undefined, error => console.error(error)
-            );
-
-        GLTF_Loader
-            .load(
-                billboard.circle.onHover,
-                (gltf) => {
-                    gltf.scene.position.set(
-                        billboard.circle.position.x,
-                        billboard.circle.position.y,
-                        billboard.circle.position.z
-                    );
-                    gltf.scene.scale.set(3, 2.5, 2.5);
-                    scene.add(gltf.scene);
-                    gltf.scene.children[0].name = `circle-${billboard.name}-onHover`;
-                    scene.children.forEach((child, index) => {
-                        if (child.children[0] && child.children[0].name == `circle-${billboard.name}-onHover`) {
-                            child.visible = false;
-                        }
-                    });
-                },
-                undefined, error => console.error(error)
-            );
-
-    }
-}
 
 
 function onWindowResize() {
@@ -243,6 +145,8 @@ function animate() {
     requestAnimationFrame(animate);
     controls.update();
     TWEEN.update();
+
+
     // console.log(camera.position)
 
     render();
@@ -266,6 +170,41 @@ const mouse = new THREE.Vector2();
 renderer.domElement.addEventListener('click', onClick, false);
 renderer.domElement.addEventListener('mousemove', onMouseMove, false);
 
+const billboards = {
+    polySurface9: { // back - center
+        name: "polySurface9",
+        animateTo: {
+            x: 0,
+            y: 0,
+            z: 7
+        }
+    },
+    polySurface8: { // back - right
+        name: "polySurface8",
+        animateTo: {
+            x: 0,
+            y: 0,
+            z: 5
+        }
+    },
+    polySurface12: { // front - center
+        name: "polySurface12",
+        animateTo: {
+            x: 0,
+            y: -1,
+            z: 13
+        }
+    },
+    "polySurface13": {
+        name: "polySurface13",
+        animateTo: {
+            x: 2,
+            y: -1,
+            z: 13
+        }
+    },
+};
+
 function onClick() {
     event.preventDefault();
 
@@ -278,21 +217,20 @@ function onClick() {
 
     if (intersects.length > 0) {
         console.log('Intersection:', intersects[0].object);
-        switch (intersects[0].object.name) {
-            case "middle":
-                new TWEEN.Tween(camera.position)
-                    .to({
-                        x: 0,
-                        y: 1,
-                        z: 19,
-                    }, animationTime)
-                    .onComplete(() => {
-                        myModal.show();
-                    })
-                    .start();
-                break;
-        }
+        handleBillboardClick(intersects[0].object);
     }
+}
+
+
+const handleBillboardClick = (obj) => {
+    let name = obj.name;
+    if (!billboards[name]) return;
+
+    camera.left = 3;
+    new TWEEN.Tween(camera.position)
+        .to(billboards[name].animateTo, animationTime)
+        // .onComplete(() => myModal.show())
+        .start();
 }
 
 
@@ -305,7 +243,6 @@ function onMouseMove() {
             children[child.children[0].name] = child;
         }
     });
-    console.log(children)
 
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
@@ -316,15 +253,7 @@ function onMouseMove() {
     if (intersects.length <= 0) return;
 
     switch (intersects[0].object.name) {
-        case "middle":
-            $("body").css("cursor", "pointer");
-            children[`circle-middle-normal`].visible = false;
-            children[`circle-middle-onHover`].visible = true;
-            break;
-        default:
-            $("body").css("cursor", "auto");
-            children[`circle-middle-normal`].visible = true;
-            children[`circle-middle-onHover`].visible = false;
+        case '':
             break;
     }
 
